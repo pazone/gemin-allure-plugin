b642b7c38c0e1116054581d008e6cee59c8390dd
pazone <pazone@yandex-team.ru>
2015-10-23 16:39:33 +0300

    fixed chaining

--- index.js
+++ index.js
@@ -10,51 +10,52 @@ var _ = require('lodash'),
 
 function AllureReporter(gemini) {
 
-    var suites = {},
-        runner;
+    var suites = {};
 
-    gemini.on('startRunner', function(geminiRunner) {
-        runner = geminiRunner;
-    });
+    gemini.on('startRunner', function(runner) {
 
-    runner.on('beginSuite', function(result) {
-        var sutePath = result.suite.path.join('/');
-        console.log(sutePath);
-        allureReporter.startSuite(sutePath);
-        suites[sutePath] = q();
-    });
+        runner.on('beginSuite', function(result) {
+            console.log('beginSuite');
+            var sutePath = result.suite.path.join('/');
+            console.log(sutePath);
+            allureReporter.startSuite(sutePath);
+            suites[sutePath] = q();
+        });
 
-    runner.on('endSuite', function(result) {
-        suites[result.suite.path.join('/')].then(function() {
-            allureReporter.endSuite();
+        runner.on('endSuite', function(result) {
+            suites[result.suite.path.join('/')].then(function() {
+                allureReporter.endSuite();
+            });
+        });
+
+        runner.on('endTest', function(result) {
+            allureReporter.startCase(result.state.name);
+            if (result.equal) {
+                allureReporter.addAttachment('actual', fs.readFileSync(result.currentPath), 'image/png');
+                allureReporter.endCase('passed');
+            } else {
+                console.log('failed test', result.state.name);
+                var currentTest = allureReporter.getCurrentSuite().currentTest;
+                allureReporter.addAttachment('actual', fs.readFileSync(result.currentPath), 'image/png');
+                allureReporter.addAttachment('expected', fs.readFileSync(result.referencePath), 'image/png');
+                var sutePath = result.suite.path.join('/');
+                suites[sutePath] = suites[sutePath].then(function() {
+                    console.log('saving diff', result.state.name);
+                    return result.saveDiffTo().then(function() {
+                        var name = writer.writeBuffer(allureReporter.options.targetDir, buffer, '.png'),
+                            attachment = new Attachment('diff', name, buffer.length, 'image/png');
+                        currentTest.addAttachment(attachment);
+                        console.log('diff done', result.state.name);
+                    });
+                }, function(e) {
+                    console.log(e);
+                });
+                allureReporter.endCase('failed', new Error('screenshots not match'));
+            }
         });
     });
 
-    runner.on('endTest', function(result) {
-        allureReporter.startCase(result.state.name);
-        if (result.equal) {
-            allureReporter.addAttachment('actual', fs.readFileSync(result.currentPath), 'image/png');
-            allureReporter.endCase('passed');
-        } else {
-            console.log('failed test', result.state.name);
-            var currentTest = allureReporter.getCurrentSuite().currentTest;
-            allureReporter.addAttachment('actual', fs.readFileSync(result.currentPath), 'image/png');
-            allureReporter.addAttachment('expected', fs.readFileSync(result.referencePath), 'image/png');
-            var sutePath = result.suite.path.join('/');
-            suites[sutePath] = suites[sutePath].then(function() {
-                console.log('saving diff', result.state.name);
-                return result.saveDiffTo().then(function() {
-                    var name = writer.writeBuffer(allureReporter.options.targetDir, buffer, '.png'),
-                        attachment = new Attachment('diff', name, buffer.length, 'image/png');
-                    currentTest.addAttachment(attachment);
-                    console.log('diff done', result.state.name);
-                });
-            }, function(e) {
-                console.log(e);
-            });
-            allureReporter.endCase('failed', new Error('screenshots not match'));
-        }
-    });
+
 
 }
 
